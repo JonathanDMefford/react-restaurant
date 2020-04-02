5115727c1e4117bc019cd3af66911357fdd105e9
Jonathan Mefford <Jonathanmefford@Breannes-MacBook-Pro.local>
2020-04-02 12:08:35 -0400

    basic requirements

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,99 +1,26 @@
 import React from 'react';
 import PageHeader from './title';
-// import SplitMenu from './splitmenu';
-// import MenuItem from './menuitem';
 import Menu from './menu';
-const axios = require('axios');
 
 
 
-class MainPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            menuItems: [],
-        };
-    }
-
-
-    async generateMenu() {
-        let result = await axios.get('https://entree-f18.herokuapp.com/v1/menu/12')
-            .then(function (response) {
-                return response.data.menu_items;
-
-                console.log(response);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .finally(function () {
-                // always executed
-            });
-        console.log(result);
-        let tempMenuItems = this.state.menuItems;
-        for (let i = 0; i < result.length; i++) {
-            tempMenuItems.push(result[i]);
-        }
-        this.setState({
-            menuItems: tempMenuItems,
-        });
-    }
-
-    async componentDidMount() {
-        let newMenuList = JSON.parse(localStorage.getItem('menu')) || [];
-        if (newMenuList.length > 0) {
-            this.setState({
-                menuItems: newMenuList,
-            });
-        } else {
-            await this.generateMenu();
-            await this.generateMenu();
-            await this.generateMenu();
-            await this.generateMenu();
-            await this.generateMenu();
-            await this.generateMenu();
-        };
-    }
-
-    componentDidUpdate() {
-        window.localStorage.setItem('menu', JSON.stringify(this.state.menuItems));
-
-    }
-
-
-    render() {
-        let appmenu = [];
-        let lunchmenu = [];
-        let mainmenu = [];
-        let sidemenu = [];
-        let dessertmenu = [];
-        if (this.state.menuItems.length == 72) {
-            for (let i = 0; i < this.state.menuItems.length; i++) {
-
-            }
-
-
-            return (
-                <div className='container mb-3'>
-                    <PageHeader />
-                    <div className='row'>
-                        <Menu menu={this.state.menuItems} title={'Appetizers'} styles={'col-6 mt-4'} amount={10} />
-                        <Menu menu={this.state.menuItems} title={'Lunch Menu'} styles={'col-6 mt-4'} amount={10} />
-                    </div>
-                    <div className='row'>
-                        {/* <Menu menu={this.state.menuItems} title={'Main Course'} styles={'col-12 mt-4'} amount={15}/> */}
-                    </div>
-                    <div className='row'>
-                        <Menu menu={this.state.menuItems} title={'Sides'} styles={'col-6 mt-4'} amount={8} />
-                        <Menu menu={this.state.menuItems} title={'Dessert'} styles={'col-6 mt-4'} amount={8} />
-                    </div>
-                </div>
-            );
-        } else {
-            return (<h1 className='text-center'>LOADING...</h1>);
-        }
-    }
+function MainPage() {
+    return (
+        <div className='container mb-3'>
+            <PageHeader />
+            <div className='row'>
+                <Menu title={'Appetizers'} styles={'col-6 mt-4'} amount={10} />
+                <Menu title={'Lunch Menu'} styles={'col-6 mt-4'} amount={10} />
+            </div>
+            <div className='row'>
+                <Menu title={'Main Course'} styles={'col-12 mt-4'} amount={15} />
+            </div>
+            <div className='row'>
+                <Menu title={'Sides'} styles={'col-6 mt-4'} amount={8} />
+                <Menu title={'Dessert'} styles={'col-6 mt-4'} amount={8} />
+            </div>
+        </div>
+    );
 }
 
 
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import MenuItem from './menuitem';
+const axios = require('axios');
 
 
 class Menu extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            menuItems: []
+        };
+    }
+
+
+    async generateMenu() {
+        let result = await axios.get('https://entree-f18.herokuapp.com/v1/menu/' + this.props.amount)
+            .then(function (response) {
+                console.log(response);
+                return response.data.menu_items;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        for (let i = 0; i < result.length; i++) {
+            console.log(result[i]);
+            result[i]['price'] = this.applyPrice()
+        }
+        console.log(result)
+        this.setState({
+            menuItems: result
+        });
+    }
+
+    async componentDidMount() {
+        let newMenuList = JSON.parse(localStorage.getItem(this.props.title)) || [];
+        if (newMenuList.length > 0) {
+            this.setState({
+                menuItems: newMenuList,
+            });
+        } else {
+            await this.generateMenu();
+        };
+    }
+
+    componentDidUpdate() {
+        window.localStorage.setItem(this.props.title, JSON.stringify(this.state.menuItems));
     }
 
 
@@ -18,11 +61,8 @@ class Menu extends React.Component {
                 <h1 className='text-center border-bottom'>{this.props.title}</h1>
                 <div>
                     <ul className='list-unstyled'>
-                        {this.props.menu.length > 0 ? Array.apply(0, Array(this.props.amount)).map((item, idx) => {
-                            {/* if (this.props.amount == 15) {
-                                console.log(this.props.menu);
-                            } */}
-                            return <MenuItem key={idx} price={this.applyPrice()} description={this.props.menu[idx].description} />
+                        {this.state.menuItems.length > 0 ? Array.apply(0, Array(this.props.amount)).map((item, idx) => {
+                            return <MenuItem key={idx} price={this.state.menuItems[idx].price} description={this.state.menuItems[idx].description} />
                         }
                         ) : null}
                     </ul>
