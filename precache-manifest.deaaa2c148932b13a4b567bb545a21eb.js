d3e03101f985b6ccd0c6d35239df913eccb1a89a
Jonathan Mefford <Jonathanmefford@Breannes-MacBook-Pro.local>
2020-04-03 08:56:31 -0400

    Updates

---
+++ precache-manifest.deaaa2c148932b13a4b567bb545a21eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6973fc71f266201446f824ca2667ca9",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "57c3de897234a09d0724",
+    "url": "/react-restaurant/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "af26f4e9b6cb80f63303",
+    "url": "/react-restaurant/static/css/main.437ceffb.chunk.css"
+  },
+  {
+    "revision": "57c3de897234a09d0724",
+    "url": "/react-restaurant/static/js/2.25257f5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-restaurant/static/js/2.25257f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af26f4e9b6cb80f63303",
+    "url": "/react-restaurant/static/js/main.64442751.chunk.js"
+  },
+  {
+    "revision": "50a373312b2be71cf905",
+    "url": "/react-restaurant/static/js/runtime-main.3783378f.js"
+  }
+]);
